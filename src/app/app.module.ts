589bacda10f503370c68c923e5c451be8ae729ed
jorisreynes <joris.reynes81@gmail.com>
2023-10-01 12:40:33 +0200

    Delete export file component + css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExportFileComponent } from './export-file/export-file.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ExportFileComponent, UploadFileComponent, NavbarComponent],
+  declarations: [AppComponent, UploadFileComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
