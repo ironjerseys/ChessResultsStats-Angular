f79e949cc0e98288ae7e883c3120fee1ce12c838
jorisreynes <joris.reynes81@gmail.com>
2024-04-06 12:30:12 +0200

    Refactor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -88,54 +88,13 @@ export class DashboardComponent implements AfterViewInit{
 			// we take all games, except those with empty accuracy
 			const allGames = data.filter((game) => game.accuracy && game.accuracy > 0);
 
-			const averageAccuracies = this.calculateEcoAccuracies(allGames);
+			// We calculate accuracy for each eco and display the data on a graph
+			const averageAccuracies = this.openingChartComponent.calculateEcoAccuracies(allGames);
 			this.openingChartComponent.displayOpeningsAccuracyChart(averageAccuracies);
 
 			// end of loading circle
 			this.isLoading = false;
 		},
 		});
-	}
-
-	calculateEcoAccuracies(games: Game[]): CommonEco[] {
-
-		// eco is the official id of an opening, we use redure to split data for each eco
-		const ecos = games.reduce((acc: Record<string, EcoData>, game: Game) => {
-
-		// if this eco doesnt exist, we create it
-		if (!acc[game.eco]) {
-			acc[game.eco] = { openings: [], totalAccuracy: 0, count: 0 };
-		}
-
-		acc[game.eco].openings.push(game.opening);
-		acc[game.eco].totalAccuracy += game.accuracy;
-		acc[game.eco].count++;
-		return acc;
-		}, {});
-
-		const commonEcos: CommonEco[] = Object.entries(ecos).map(([eco, data]) => {
-		const commonPrefix = this.findCommonPrefix(data.openings);
-		return {
-			eco,
-			opening: commonPrefix || eco, // Utiliser eco comme fallback si aucun préfixe commun n'est trouvé
-			averageAccuracy: data.totalAccuracy / data.count,
-			count: data.count, // Vous pouvez inclure count si vous prévoyez de l'utiliser pour d'autres traitements
-		};
-		});
-		return commonEcos.sort((a, b) => b.count - a.count).slice(0, 20);
-	}
-
-	// Fonction auxiliaire pour trouver le préfixe commun dans un tableau de strings
-	findCommonPrefix(strings: string[] | undefined): string {
-		if (!strings || strings.length === 0) return '';
-
-		let prefix = strings[0];
-		for (let i = 1; i < strings.length; i++) {
-		while (strings[i].indexOf(prefix) !== 0) {
-			prefix = prefix.substring(0, prefix.length - 1);
-			if (prefix === '') return '';
-		}
-		}
-		return prefix;
 	}
 }
--- src/app/opening-chart/opening-chart.component.ts
+++ src/app/opening-chart/opening-chart.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import { Game } from '../models/game';
+import { EcoData, CommonEco } from '../models/eco-data.model';
 Chart.register(...registerables);
 
 @Component({
@@ -10,6 +12,46 @@ Chart.register(...registerables);
 export class OpeningChartComponent {
 
 	openingsAccuracyChart: Chart | undefined;
+
+	calculateEcoAccuracies(games: Game[]): CommonEco[] {
+		// eco is the official id of an opening, we use reduce to split data for each eco
+		const ecos = games.reduce((acc: Record<string, EcoData>, game: Game) => {
+			// if this eco doesn't exist, we create it
+			if (!acc[game.eco]) {
+				acc[game.eco] = { openings: [], totalAccuracy: 0, count: 0 };
+			}
+	
+			acc[game.eco].openings.push(game.opening);
+			acc[game.eco].totalAccuracy += game.accuracy;
+			acc[game.eco].count++;
+			return acc;
+		}, {});
+	
+		// Fonction auxiliaire pour trouver le préfixe commun dans un tableau de strings, maintenant intégrée
+		const findCommonPrefix = (strings: string[] | undefined): string => {
+			if (!strings || strings.length === 0) return '';
+	
+			let prefix = strings[0];
+			for (let i = 1; i < strings.length; i++) {
+				while (strings[i].indexOf(prefix) !== 0) {
+					prefix = prefix.substring(0, prefix.length - 1);
+					if (prefix === '') return '';
+				}
+			}
+			return prefix;
+		};
+	
+		const commonEcos: CommonEco[] = Object.entries(ecos).map(([eco, data]) => {
+			const commonPrefix = findCommonPrefix(data.openings);
+			return {
+				eco,
+				opening: commonPrefix || eco, // Utiliser eco comme fallback si aucun préfixe commun n'est trouvé
+				averageAccuracy: data.totalAccuracy / data.count,
+				count: data.count, // Vous pouvez inclure count si vous prévoyez de l'utiliser pour d'autres traitements
+			};
+		});
+		return commonEcos.sort((a, b) => b.count - a.count).slice(0, 20);
+	}
 	
 	displayOpeningsAccuracyChart(averageAccuracies: any) {
 		if (averageAccuracies == undefined) {
