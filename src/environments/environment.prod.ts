dcfed7e09ef62309a2eb63d141c1c88b84f14776
Joris Reynes <jo.r6349@gmail.com>
2024-11-24 15:55:11 +0100

    update base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiBaseUrl:
-        'https://chessresultsstatscsharp-hqemdgbjcbesbwd8.francecentral-01.azurewebsites.net/api/Games',
+        'https://chessresultsstatscsharp-hqemdgbjcbesbwd8.francecentral-01.azurewebsites.net/api',
     routes: {
         games: '/Games',
         winrates: '/Winrates',
