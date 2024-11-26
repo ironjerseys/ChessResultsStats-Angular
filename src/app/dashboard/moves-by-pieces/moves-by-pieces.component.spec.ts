386e71d8f32f6d7522b6c97691ffc1f619116545
Joris Reynes <jo.r6349@gmail.com>
2024-11-26 22:57:46 +0100

    css

---
+++ src/app/dashboard/moves-by-pieces/moves-by-pieces.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovesByPiecesComponent } from './moves-by-pieces.component';
+
+describe('MovesByPiecesComponent', () => {
+  let component: MovesByPiecesComponent;
+  let fixture: ComponentFixture<MovesByPiecesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MovesByPiecesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MovesByPiecesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dashboard/moves-by-pieces/moves-by-pieces.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moves-by-pieces',
+  standalone: true,
+  imports: [],
+  templateUrl: './moves-by-pieces.component.html',
+  styleUrl: './moves-by-pieces.component.css'
+})
+export class MovesByPiecesComponent {
+
+}
