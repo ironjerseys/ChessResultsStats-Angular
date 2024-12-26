254620693179960839e7c665712be643016a9376
Joris Reynes <jo.r6349@gmail.com>
2024-12-26 18:25:45 +0100

    css html cleaning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: 'dashboard', component: DashboardComponent },
-];
+const routes: Routes = [{ path: '', component: DashboardComponent }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
